e4628776ab3a4f4c8f93c1fd5347ea63be9b6786
kondo <kondo@local>
2022-01-25 18:24:02 +0900

    feat: バリデーションエラー時の処理を追加 (暫定)

--- 02_users_controller.php
+++ 02_users_controller.php
@@ -10,7 +10,13 @@ class UsersController extends AppControlelr
     $userData['invitationCode'] = $this->params['data']['invitationCode'];
 
     $service = new UserService($this);
-    $user = $service->register($userData);
+
+    try {
+      $user = $service->register($userData);
+    } catch (\Exception $e) {
+      // TODO: 仕様未確定のため暫定
+      throw $e;
+    }
 
     // 20211225 メルマガオプトアウトフラグを追加
     $user['mailMagazineOptedIn'] = $this->Session->read('mailMagazineOptedIn');
